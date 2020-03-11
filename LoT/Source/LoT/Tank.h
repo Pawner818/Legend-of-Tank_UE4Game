d262f398a515246796de24bbdc944d131d12cea0
Zakhar Hurov <gurov.zahar@gmail.com>
2020-03-11 18:41:44 +0100

    19. Unify Player & AI Aiming
    AITank now aiming at us every frame

--- LoT/Source/LoT/Tank.h
+++ LoT/Source/LoT/Tank.h
@@ -15,6 +15,8 @@ public:
 	// Sets default values for this pawn's properties
 	ATank();
 
+	void AimAt(FVector HitLocation);
+
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
