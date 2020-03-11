276a494b62a650a84bc5df3b3c32b3783b9dc2db
Zakhar Hurov <gurov.zahar@gmail.com>
2020-03-11 18:25:18 +0100

    14. Addeed Tick() method to PlayerController

--- LoT/Source/LoT/TankPlayerController.cpp
+++ LoT/Source/LoT/TankPlayerController.cpp
@@ -4,7 +4,36 @@
 #include "LoT.h"
 
 
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	auto ControlledTank = GetControlledTank();
+	if (!ControlledTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PlayController not possesing a tank"));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController possessing: %s"), *(ControlledTank->GetName()));
+	}
+}
+
+void ATankPlayerController::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	AimTowardsCrosshair();
+}
+
 ATank* ATankPlayerController::GetControlledTank() const
 {
 	return Cast<ATank>(GetPawn());
+}
+
+void ATankPlayerController::AimTowardsCrosshair()
+{
+	if (!GetControlledTank()) { return; }
+
+	// Get world location if linetrace through crosshair
+	// If it hits the landscape
+		// Tell controlled tank to aim at this point
 }
--- LoT/Source/LoT/TankPlayerController.h
+++ LoT/Source/LoT/TankPlayerController.h
@@ -16,4 +16,12 @@ class LOT_API ATankPlayerController : public APlayerController
 public:
 
 	ATank* GetControlledTank() const;
+
+	virtual void BeginPlay() override;
+
+	virtual void Tick(float DeltaTime) override;
+
+	// Start the tank moving the barrel so that a shot would hit where
+	// the crosshair intersects the world
+	void AimTowardsCrosshair();
 };
