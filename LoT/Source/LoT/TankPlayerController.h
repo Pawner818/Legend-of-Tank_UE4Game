a911c71a3075a65446c377abe2c386d09bf5bff0
Zakhar Hurov <gurov.zahar@gmail.com>
2020-03-11 18:30:37 +0100

    16. Finding Screen Pixel Coordinates for aiming system

--- LoT/Source/LoT/TankPlayerController.h
+++ LoT/Source/LoT/TankPlayerController.h
@@ -13,7 +13,7 @@ class LOT_API ATankPlayerController : public APlayerController
 {
 	GENERATED_BODY()
 
-public:
+private:
 
 	ATank* GetControlledTank() const;
 
@@ -27,4 +27,10 @@ public:
 
 	// Return an OUT parameter, true if hit landscape
 	bool GetSightRayHitLocation(FVector& HitLocation) const;
+
+	UPROPERTY(EditAnywhere)
+		float CrosshairXLocation = 0.5;
+
+	UPROPERTY(EditAnywhere)
+		float CrosshairYLocation = 0.3333;
 };
