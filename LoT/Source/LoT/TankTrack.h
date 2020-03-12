c4c85ed290fe002d1ccff64eef73bc53332cf480
Zakhar Hurov <gurov.zahar@gmail.com>
2020-03-12 15:41:11 +0100

    31 ApplyForceAtLocation() in Action
    Well we added a Force and casted it to TankRoot (GetOwner()-> GetRootComponent() ).

--- LoT/Source/LoT/TankTrack.h
+++ LoT/Source/LoT/TankTrack.h
@@ -14,7 +14,11 @@ class LOT_API UTankTrack : public UStaticMeshComponent
 
 public:
 
+	// Sets the throttle between 1 and -1 
 	UFUNCTION (BlueprintCallable, Category = Input)
 	void SetThrottle(float Throttle);
 	
+	// Max Force per Track
+	UPROPERTY(EditDefaultsOnly)
+	float TrackMaxDrivingForce = 400000.f; // Assume 40 tonne tank, acceleration = 1g.
 };
