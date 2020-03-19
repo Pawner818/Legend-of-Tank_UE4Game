7cc97ce18b1033d29c3875edb97423a4fb8c8895
Zakhar Hurov <gurov.zahar@gmail.com>
2020-03-19 19:47:49 +0100

    51. A mortar was added as a BP_Mortar (Pawn). The mortar can aim at us and shoot

--- LoT/Source/LoT/TankAimingComponent.cpp
+++ LoT/Source/LoT/TankAimingComponent.cpp
@@ -106,7 +106,7 @@ void UTankAimingComponent::MoveBarrelTowards(FVector AimDirections)
 
 	// Rotation logic
 	Barrel->Elevate(DeltaRotator.Pitch);
-	if (DeltaRotator.Yaw < 180)
+	if (FMath::Abs(DeltaRotator.Yaw) < 180)
 	{
 		Turret->Rotate(DeltaRotator.Yaw);
 	}
