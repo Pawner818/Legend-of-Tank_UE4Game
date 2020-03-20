90d897cf8d537be1a9c2091d1de90df65680b3ca
Zakhar Hurov <gurov.zahar@gmail.com>
2020-03-20 16:11:52 +0100

    53. The Particle system was successfully implemented
    + some audio from the Starter Content were added

--- LoT/Source/LoT/Projectile.h
+++ LoT/Source/LoT/Projectile.h
@@ -22,12 +22,12 @@ public:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-	// Called every frame
-	virtual void Tick(float DeltaSeconds) override;
-
 	void LaunchProjectile(float Speed);
 
 private:
+
+	UFUNCTION()
+	void OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, FVector NormalImpulse, const FHitResult& Hit);
 
 	UProjectileMovementComponent* ProjectileMovement = nullptr;
 
@@ -36,4 +36,9 @@ private:
 
 	UPROPERTY(VisibleAnywhere, Category = "Components")
 	UParticleSystemComponent* LaunchBlast = nullptr;
+
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	UParticleSystemComponent* ImpactBlast = nullptr;
+
+	
 };
